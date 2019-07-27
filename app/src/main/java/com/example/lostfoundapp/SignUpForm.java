b9c2fc6bab8035cefada1bb6dff91fe29d3411e1
Shivani Dhiman <shivanidhiman1619@gmail.com>
2019-07-27 19:16:06 -0400

    [Shivani]: Initial commit

---
+++ app/src/main/java/com/example/lostfoundapp/SignUpForm.java
@@ -0,0 +1,14 @@
+package com.example.lostfoundapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_form);
+    }
+}
