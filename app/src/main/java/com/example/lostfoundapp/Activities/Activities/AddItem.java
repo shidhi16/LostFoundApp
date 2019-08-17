8d9101450b2779626455e96c0cf68f6277b35d6b
Shivani Dhiman <shivanidhiman1619@gmail.com>
2019-08-17 19:31:33 -0400

    [Shivani] : Add Item Customisation

--- app/src/main/java/com/example/lostfoundapp/Activities/Activities/AddItem.java
+++ app/src/main/java/com/example/lostfoundapp/Activities/Activities/AddItem.java
@@ -1,15 +1,29 @@
 package com.example.lostfoundapp.Activities.Activities;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.lostfoundapp.Activities.pojoUsers.Items;
+import com.example.lostfoundapp.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddItem extends AppCompatActivity {
+
+    ArrayList<itemsList>  mItemsList;
 
     public AddItem(ImageView aItemImg, Spinner statusSpinner, TextView aItemName, DatePicker aDate, TextView aDescription, TextView aLocation, TextView aContact, Spinner statusSpinner1, List<String> varSpinnerData, float varScaleX, float varScaleY) {
         this.aItemImg = aItemImg;
@@ -29,8 +43,40 @@ public class AddItem extends AppCompatActivity {
     TextView aDescription;
     TextView aLocation;
     TextView aContact;
+    Button btnPost;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.additem_activity);
 
+        mItemsList = new ArrayList<>();
 
+        Button buttonPost = findViewById(R.id.btnPost);
+        buttonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
 
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(mItemsList);
+        editor.putString("item add task", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("item add task", null);
+        Type type = new TypeToken<ArrayList<>>() {}.getType();
+        mItemsList = gson.fromJson(json, type);
+
+    }
 }
